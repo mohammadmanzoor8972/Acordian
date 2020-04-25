17c8452dbd4541ba10b89f96d121d15e3cc81114
mohammad.manzoor8972@gmail.com <malam@LKNDSS0NRG3QD.local>
2020-04-25 07:18:07 +0530

    remove header

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
       <div className="App">
-        <h1>Hello Acrodian</h1>
       </div>
       <Acordian title="Profile" expand={true}>
         I am gogin to display multiple attribute
